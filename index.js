0ec2372920721d217d87f4f67f82020e6888c0c6
Khorram Khondkar <khorramk.kbsk@gmail.com>
2023-12-13 13:09:15 +0000

    Mapped product data to required format.

--- index.js
+++ index.js
@@ -16,13 +16,47 @@ function init () {
 function fetchProductsFromShopify () {
     const shopifyUrl = '';
     
-    fetch(shopifyUrl).then(function (response) {
-        return response.json();
-    }).then(function (response) {
-        return response.products
-    }).catch(function (err) {
-        console.error(err.message);
-    })
+    fetch(shopifyUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(response) {
+            return mapDataFromProducts(response.products)
+        })
+        .then(function (mappedProductData) {
+            return //TODO - convert to html
+        })
+        .catch((err) => {
+            console.error(err);
+        })
 
     
+}
+
+function mapDataFromProducts(products) {
+    return products.map(function (product) {
+        return {
+            productTitle: product.title,
+            customerGender: extractGenderFromTags(product.tags),
+            productPrice: getPriceFromFirstVariant(product.variants),
+            productPageLink: product.handle,
+            productImageSrc: findTheCorrectImage(product)
+        }
+    })
+}
+
+function extractGenderFromTags(tags) {
+    return tags[0]?.split(' ').find(function (word) {
+        return word.includes('mens') || word.includes('woman');
+    }) ?? '';
+}
+
+function getPriceFromFirstVariant(varaints) {
+    return varaints[0].price || 'Price not listed';
+}
+
+function findTheCorrectImage(product) {
+    return product.images.find(function (image) {
+        return image.product_id === product.id;
+    });
 }
