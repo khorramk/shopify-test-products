064fbd1f3230005a1a8f7442cf0cf12fb5b50b5c
Khorram Khondkar <khorramk.kbsk@gmail.com>
2023-12-13 11:30:03 +0000

    writing initial function

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 //make functions
 //fetch products
 //convert products data to html contents for the browser
+
+/**
+ * This is a inital starter function.
+ * 
+ * @return void 
+ *
+ */
+
+function init () {
+    fetchProductsFromShopify()
+}
+
+function fetchProductsFromShopify () {
+//   TODO:
+//    fetch product from shopify
+}
